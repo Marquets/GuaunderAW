b9eb01c6a8fc99959eaa734889c065703a96c95d
samgar01 <samugarcia9696@gmail.com>
2017-05-23 20:21:32 +0200

    Unificado css y arreglado para visualización correcta. Comentadas funciones de chats.js y pagPrincipal.js

--- public/js/chats.js
+++ public/js/chats.js
@@ -1,3 +1,4 @@
+//activación de los tooltip (descripción cuando se pasa por encima del botón para crear un nuevo mensaje)
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
 });
--- public/js/pagPrincipal.js
+++ public/js/pagPrincipal.js
@@ -1,11 +1,18 @@
-$("#star-btn").click(function(){
-    $("#carousel-usuario").carousel("next");
+//avance del carrusel cuando el botón de SuperLike es pulsado
+$(document).ready(function() {
+	$("#star-btn").click(function(){
+    	$("#carousel-usuario").carousel("next");
+	});
 });
-
-$("#heart-btn").click(function(){
-    $("#carousel-usuario").carousel("next");
+//avance del carrusel cuando el botón de MeGusta es pulsado
+$(document).ready(function() {
+	$("#heart-btn").click(function(){
+	    $("#carousel-usuario").carousel("next");
+	});
 });
-
-$("#cross-btn").click(function(){
-    $("#carousel-usuario").carousel("next");
+//avance del carrusel cuando el botón de NoMeGusta es pulsado
+$(document).ready(function() {
+	$("#cross-btn").click(function(){
+	    $("#carousel-usuario").carousel("next");
+	});
 });
